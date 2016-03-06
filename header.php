41f54d0d7ad33b89fa4f44e9c09217155858e3b4
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-06 08:58:31 +0200

    navbar fixes

--- header.php
+++ header.php
@@ -25,31 +25,16 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<nav id="site-navigation" class="navbar main-navigation" role="navigation">
+			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
 			<?php wp_nav_menu( array(
 				'menu'              => 'primary',
 				'theme_location'    => 'primary',
 				'menu_id'           => 'primary-menu',
 				'depth'             => 2,
-				'container'         => 'div',
-				'container_class'   => 'navbar',
+				'container'         => '',
+				// 'container_class'   => 'navbar',
 				'container_id'      => '',
 				'menu_class'        => 'nav navbar-nav',
 				'fallback_cb'       => 'Shoestrap_Navwalker::fallback',
